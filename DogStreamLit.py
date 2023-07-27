97f292a033bfb597778ca1d784e306ab6b8b5d42
venomoose <140755703+venomoose@users.noreply.github.com>
2023-07-27 16:33:34 -0400

    Update DogStreamLit.py

--- DogStreamLit.py
+++ DogStreamLit.py
@@ -3,4 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('2017.csv')
+st.set_page_config(page_title = 'Dog Breeds', layout = 'wide')
+
+st.title('Streamlit Demo Using Dog Breed Data Set')
+st.write(df)
 
